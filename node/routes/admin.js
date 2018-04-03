df3a2b87ac99597ac1b497d6fadb5b1b3453c5d8
Austin Guo <austinguo550@gmail.com>
2018-04-03 00:08:51 -0700

    Add route to grab top 3 winners

--- node/routes/admin.js
+++ node/routes/admin.js
@@ -39,4 +39,48 @@ router.post('/passed', function(req, res) {
 
 });
 
+
+router.post('/winners', function(req, res) {
+    console.log("getting the top 3 scoring winners who passed the challenge");
+
+    let password = req.body.password;
+    if (password === PASSWORD) {
+        User.find({}, function(err, users) {
+            if (err) throw err;
+    
+            console.log("all users", users);    // debug purposes only
+            let passing_users = [];
+            // find users that meet requirements:
+                // played over 100 games
+                // 20% win rate
+            for (i = 0; i < users.length; i++) {
+                if (users[i].data.games > 100 && users[i].data.won/users[i].data.games > 0.20) {
+                    console.log(users[i]);
+                    passing_users.push(users[i]);
+                }
+            }
+
+            passing_users.sort(predicateBy("score"));
+
+            res.send(passing_users.slice(1,4));
+        });
+    }
+    else {
+        res.status(401).send('You must be a UPE officer to access inductee data');
+    }
+});
+
+
+function predicateBy(prop) {
+    return function(a, b) {
+        if (a[prop] > b[prop]) {
+            return 1;
+        }
+        else if (a[prop] < b[prop]) {
+            return -1;
+        }
+        return 0;
+    }
+}
+
 module.exports = router;
